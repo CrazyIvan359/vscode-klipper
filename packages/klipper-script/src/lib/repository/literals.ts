491dec2c0f4f2485a4ac14c04c7f1ecde69a4b21
Michael Murton <6764025+crazyivan359@users.noreply.github.com>
2026-01-02 00:32:37 -0500

    fix stringLiteral begins ignoring escaped quotes

--- packages/klipper-script/src/lib/repository/literals.ts
+++ packages/klipper-script/src/lib/repository/literals.ts
@@ -11,16 +11,32 @@ export const nullLiteral: TMGrammarScope = {
 };
 
 export const stringLiteral: TMGrammarScope = {
-	name: "string.klipper-script",
-	begin: /(["'])/,
-	beginCaptures: {
-		1: { name: "punctuation.definition.string.begin.klipper-script" },
-	},
-	end: /(?<!\\)(\1)|\\\\(\1)/,
-	endCaptures: {
-		1: { name: "punctuation.definition.string.end.klipper-script" },
-		2: { name: "punctuation.definition.string.end.klipper-script" },
-	},
+	patterns: [
+		{
+			name: "string.klipper-script",
+			begin: /(?<!\\)(')|\\\\(')/,
+			beginCaptures: {
+				1: { name: "punctuation.definition.string.begin.klipper-script" },
+			},
+			end: /(?<!\\)(')|\\\\(')/,
+			endCaptures: {
+				1: { name: "punctuation.definition.string.end.klipper-script" },
+				2: { name: "punctuation.definition.string.end.klipper-script" },
+			},
+		},
+		{
+			name: "string.klipper-script",
+			begin: /(?<!\\)(")|\\\\(")/,
+			beginCaptures: {
+				1: { name: "punctuation.definition.string.begin.klipper-script" },
+			},
+			end: /(?<!\\)(")|\\\\(")/,
+			endCaptures: {
+				1: { name: "punctuation.definition.string.end.klipper-script" },
+				2: { name: "punctuation.definition.string.end.klipper-script" },
+			},
+		},
+	]
 };
 
 export const numericLiteral: TMGrammarScope = {
